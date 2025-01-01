bcd34147657983313f990eb6b657e5b8ac714f64
Alim Mahmudov <alimmahmudov27@gmail.com>
2025-01-01 21:31:51 +0600

    sort

--- src/appSections/homePage/home/sort/Sort.tsx
+++ src/appSections/homePage/home/sort/Sort.tsx
@@ -1,7 +1,70 @@
+import { useFileSort } from "@/stores/SortStores";
 import React from "react";
 
 const Sort = () => {
-  return <div></div>;
+  const {
+    selectType,
+    selectCategory,
+    setSelectCategory,
+    setSelectType,
+    resetFile,
+  } = useFileSort();
+
+  const TypeClick = (type: string) => {
+    setSelectType(selectType === type ? "" : type);
+  };
+
+  const CategoryClick = (type: string) => {
+    setSelectCategory(selectCategory === type ? "" : type);
+  };
+
+  const buttonStyle = (isSelected: boolean) => ({
+    background: isSelected ? "#007bff" : "#e0e0e0",
+    color: isSelected ? "#fff" : "#000",
+    padding: "10px 20px",
+    margin: "5px",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+  });
+
+  return (
+    <div>
+      <button
+        style={buttonStyle(selectType === "stories")}
+        onClick={() => TypeClick("stories")}
+      >
+        stories
+      </button>
+      <button
+        style={buttonStyle(selectType === "classic")}
+        onClick={() => TypeClick("classic")}
+      >
+        classic
+      </button>
+      <button
+        style={buttonStyle(selectType === "fantasy")}
+        onClick={() => TypeClick("fantasy")}
+      >
+        fantasy
+      </button>
+      <button
+        style={buttonStyle(selectCategory === "domestic")}
+        onClick={() => CategoryClick("domestic")}
+      >
+        Отечественные
+      </button>
+      <button
+        style={buttonStyle(selectCategory === "foreign")}
+        onClick={() => CategoryClick("foreign")}
+      >
+        Зарубежные
+      </button>
+      <button onClick={resetFile} style={buttonStyle(false)}>
+        Все
+      </button>
+    </div>
+  );
 };
 
 export default Sort;
