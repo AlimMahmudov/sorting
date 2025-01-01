b417dca9b0638ffbb8cd677569ea098407df5e7b
Alim Mahmudov <alimmahmudov27@gmail.com>
2025-01-01 21:36:00 +0600

    sort home

--- src/appSections/homePage/home/Home.tsx
+++ src/appSections/homePage/home/Home.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+"use client";
+import { useFileSort } from "@/stores/SortStores";
+import { useData } from "./data/Data";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { selectType, selectCategory } = useFileSort(); // Destructure selectedType and selectedCategory
+  const { data } = useData(); // Assuming data contains your products
+
+  const filteredData = data.filter((el) => {
+    const matchesType = selectType ? el.type === selectType : true;
+    const matchesCategory = selectCategory
+      ? el.category === selectCategory
+      : true;
+    return matchesType && matchesCategory;
+  });
+
+  return (
+    <div>
+      {filteredData.length === 0 ? (
+        <p>No products found</p> // Display a message if no products match the filters
+      ) : (
+        filteredData.map((el, index) => (
+          <div className="card" key={index}>
+            <h1>{el.title}</h1>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Home;
--- src/appSections/homePage/home/sort/Sort.tsx
+++ src/appSections/homePage/home/sort/Sort.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useFileSort } from "@/stores/SortStores";
 import React from "react";
 
